911d37d044eb0ba7658b37267c195419f69bd554
yearnings <doki@dotirr.ru>
2026-09-05 13:41:27 +0700

    Добавлен импорт flutter/material.dart в storage_service.dart для ThemeMode и ValueNotifier

--- app/lib/services/storage_service.dart
+++ app/lib/services/storage_service.dart
@@ -1,5 +1,6 @@
 // Сервис локального хранения настроек и офлайн-кэша
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/models.dart';
 
